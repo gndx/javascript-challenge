1b32a4d5fccc135f5d1b9d613d6cddb615f7faca
Adriana Martinez <adpmb1995@hotmail.com>
2020-06-08 17:43:09 -0500

    Resolved second problem

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = "https://rickandmortyapi.com/api/character/";
-var xhttp = new XMLHttpRequest();
+const API = "https://rickandmortyapi.com/api/character/";
+const request = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200){
-        callback(null, JSON.parse(xhttp.responseText));
+const fetchData = (urlApi, callback) => {
+  request.onreadystatechange = event => {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        callback(null, JSON.parse(request.responseText));
       } else {
-        return callback(url_api);
+        return callback(urlApi);
       }
     }
   };
-  xhttp.open("GET", url_api, false);
-  xhttp.send();
-}
+  request.open("GET", urlApi, false);
+  request.send();
+};
 
 fetchData(API, function(error1, data1) {
-  if (error1) return console.error("Error" + " " + error1);
+  if (error1) return console.error(`Error ${error1}`);
   console.log("Primer Llamado...");
   fetchData(API + data1.results[0].id, function(error2, data2) {
     if (error2) return console.error(error1);
@@ -26,9 +26,9 @@ fetchData(API, function(error1, data1) {
     fetchData(data2.origin.url, function(error3, data3) {
       if (error3) return console.error(error3);
       console.log("Tercero Llamado...");
-      console.log("Personajes:" + " " + data1.info.count);
-      console.log("Primer Personaje:" + " " + data2.name);
-      console.log("Dimensión:" + " " + data3.dimension);
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
