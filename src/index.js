f510b5d713c22b3337752d9cdb29cd20c8d72f5b
WILMAR MALAVER <yoamalaver@gmail.com>
2020-06-09 22:45:32 +0200

    templates string

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/';
+const xhttp = new XMLHttpRequest();
 
 function fetchData(url_api, callback) {
   xhttp.onreadystatechange = function (event) {
@@ -17,16 +17,16 @@ function fetchData(url_api, callback) {
 
 fetchData(API, function (error1, data1) {
   if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
+  console.log(`Primer Llamado...`)
   fetchData(API + data1.results[0].id, function (error2, data2) {
     if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
+    console.log(`Segundo Llamado...`)
     fetchData(data2.origin.url, function (error3, data3) {
       if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Tercero Llamado...`)
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
