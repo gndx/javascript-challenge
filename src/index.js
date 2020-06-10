8bc4b62f0f4a8527b777b261cbaef6baee9c3b50
WILMAR MALAVER <yoamalaver@gmail.com>
2020-06-10 09:42:33 +0200

    ECMAScript6(ES6)

--- src/index.js
+++ src/index.js
@@ -3,30 +3,39 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = 'https://rickandmortyapi.com/api/character/';
 const xhttp = new XMLHttpRequest();
 
-const fetchData = (url_api, callback)  => {
-  xhttp.onreadystatechange =  (event) => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200)
-        callback(null, JSON.parse (xhttp.responseText));
-      else return callback(url_api);
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    xhttp.open('GET', url_api, true);
+    xhttp.send();
+    xhttp.onreadystatechange = event => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200)
+          resolve(JSON.parse(xhttp.responseText));
+      else {
+        return reject(`Ocurrió un error`);
+      }
     }
   };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
+})
+}
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log(`Primer Llamado...`)
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log(`Segundo Llamado...`)
-    fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log(`Tercero Llamado...`)
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
-  });
-});
+    
+Promise
+  .all([fetchData(API)])
+  .then(data1 => {
+  console.log(`'Personajes: ${data1[0].info.count}`);
+  return fetchData(`${API}${data1[0].results[0].id}`)
+  
+})
+  
+.then(data2 => {
+  console.log(`Primer Personaje: ${data2.name}`);
+  return fetchData(data2.origin.url)
+})
+  
+.then(data3 => {
+  console.log(`Dimensión: ${data3.dimension}`);
+})
+
+.catch(message => console.log(message))
+  
