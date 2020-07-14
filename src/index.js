5ac426706f1b40760e020384c57760bf6cf7d10a
Alfonso Neil Jimenez Casallas <alfonso@gradiweb.com>
2020-07-14 15:15:27 -0500

    Eliminando código comentado innecesario

--- src/index.js
+++ src/index.js
@@ -36,20 +36,3 @@ fetchData(`${API}`).then(data1 => {
   console.log(error)
 })
 
-/*
-fetchData(`${API}`,(error1, data1) => {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log(`Primer Llamado...`)
-  fetchData(`${API}${data1.results[0].id}`,(error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log(`Segundo Llamado...`)
-    fetchData(data2.origin.url,(error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log(`Tercero Llamado...`)
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
-  });
-});
-*/
