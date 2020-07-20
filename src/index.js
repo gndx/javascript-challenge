b3c64144f4cff6b7484aa9979a8dd8622faa83ce
Juan Gonzalez <j3045771061@gmail.com>
2020-07-19 23:45:54 -0500

    [RESOLVE ERROR]

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
+  let xhttp = new XMLHttpRequest();
+  xhttp.open('GET', url_api, true);
   xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
+    if (xhttp.readyState === 4) {
+      if (xhttp.status == 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      }
+      else {
+        const error = new Error('Wooops! ' + url_api)
+        return callback(error, null);
+      }
     }
   };
-  xhttp.open('GET', url_api, false);
   xhttp.send();
 };
 
@@ -19,7 +23,7 @@ fetchData(API, function (error1, data1) {
   if (error1) return console.error('Error' + ' ' + error1);
   console.log('Primer Llamado...')
   fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
+    if (error2) return console.error(error2);
     console.log('Segundo Llamado...')
     fetchData(data2.origin.url, function (error3, data3) {
       if (error3) return console.error(error3);
