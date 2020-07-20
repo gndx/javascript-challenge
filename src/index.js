2c32f3ab47f55f5eb8dd2d79a30684721cfd6d6c
Juan Gonzalez <j3045771061@gmail.com>
2020-07-19 23:51:20 -0500

    [RESOLVE ERROR2] ES6

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ let API = 'https://rickandmortyapi.com/api/character/';
 function fetchData(url_api, callback) {
   let xhttp = new XMLHttpRequest();
   xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = function (event) {
+  xhttp.onreadystatechange = event => {
     if (xhttp.readyState === 4) {
       if (xhttp.status == 200) {
         callback(null, JSON.parse(xhttp.responseText));
@@ -19,18 +19,18 @@ function fetchData(url_api, callback) {
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(`Error ${error1}`);
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+  fetchData(API + data1.results[0].id, (error2, data2) => {
     if (error2) return console.error(error2);
     console.log('Segundo Llamado...')
     fetchData(data2.origin.url, function (error3, data3) {
       if (error3) return console.error(error3);
       console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
