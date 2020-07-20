2b06ba91dcaccf6dc175cc44e3a473f0a8a82e9b
Juan Gonzalez <j3045771061@gmail.com>
2020-07-20 18:34:32 -0500

    [ASYNC/AWAIT] change the promiese in async/await succesfull

--- src/index.js
+++ src/index.js
@@ -2,43 +2,100 @@ let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 let API = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(url_api) {
-  let xhttp = new XMLHttpRequest();
+const fetchData = url_api => {
+  const xhttp = new XMLHttpRequest();
   return new Promise((resolve, reject) => {
     xhttp.open('GET', url_api, true);
-    xhttp.onreadystatechange = event => {
+    xhttp.onreadystatechange = (event => {
       if (xhttp.readyState === 4) {
         if (xhttp.status == 200) {
           resolve(JSON.parse(xhttp.responseText));
         }
         else {
-          const error = new Error('Wooops! ' + url_api)
-          return reject(error);
+          reject(new Error('Wooops! ' + url_api));
         }
       }
-    };
+    });
     xhttp.send();
   })
 };
-
-fetchData(API)
-  .then(response => {
+const fetchD1 = async (url_api) => {
+  try {
+    const response = await fetchData(url_api)
+    const data = await fetchData(`${API}${response.results[0].id}`)
+    const origin = await fetchData(data.origin.url)
     console.log('Primer Llamado...')
     console.log(`Personajes: ${response.info.count}`);
-    return fetchData(API + response.results[0].id)
-  })
-  .then(response => {
     console.log('Segundo Llamado...')
-    console.log(`Primer Personaje: ${response.name}`);
-    return fetchData(response.origin.url)
-
-  })
-  .then(response => {
+    console.log(`Primer Personaje: ${data.name}`);
     console.log('Tercero Llamado...')
-    console.log(`Dimensión: ${response.dimension}`);
-    return fetchData(API)
-  })
-  .catch(error => console.error(`Error ${error}`))
+    console.log(`Dimensión: ${origin.dimension}`);
+  } catch (error) {
+    console.error(error)
+  }
+
+}
+
+//const fetchD2 = async (url_api) => {
+//  try {
+//    const response = await fetchData(url_api)
+//    console.log('Segundo Llamado...')
+//    console.log(`Primer Personaje: ${response.results[4].name}`);
+//  } catch (error) {
+//    console.error(error)
+//  }
+//}
+//
+//const fetchD3 = async (url_api) => {
+//  try {
+//    const response = await fetchData(url_api)
+//    console.log('Tercero Llamado...')
+//    console.log(`Dimensión: ${response.dimension}`);
+//  } catch (error) {
+//    console.error(error)
+//  }
+//}
+//const fechD2 = async () => {
+//const data = await fetchData(API + response.result[0].id)
+//console.log(`Primer Personaje: ${data.response.name}`);
+
+//console.log('Primer Llamado...')
+//return response.results[0].id
+//  try {
+//    const response = await fetchData(API + data.results[0].id)
+//    console.log('Segundo Llamado...')
+//    console.log(`Primer Personaje: ${response.name}`, data);
+//  } catch{
+//
+//  }
+//}
+//const fechD3 = async () => {
+//  const response = await fetchData(API)
+//  //const data = await response.results[0].id
+//  console.log('Tercero Llamado...')
+//  console.log(`Dimensión: ${response.dimension}`);
+//  return fetchData(API)
+//}
+fetchD1(API)
+
+//fetchData(API)
+//  .then(response => {
+//    console.log('Primer Llamado...')
+//    console.log(`Personajes: ${response.info.count}`);
+//    return fetchData(API + response.results[0].id)
+//  })
+//  .then(response => {
+//    console.log('Segundo Llamado...')
+//    console.log(`Primer Personaje: ${response.name}`);
+//    return fetchData(response.origin.url)
+//
+//  })
+//  .then(response => {
+//    console.log('Tercero Llamado...')
+//    console.log(`Dimensión: ${response.dimension}`);
+//    return fetchData(API)
+//  })
+//  .catch(error => console.error(`Error ${error}`))
 
 // fetchData(API, (error1, data1) => {
 //  if (error1) return console.error(`Error ${error1}`);
