f2c3f25251811dc6b333bc341ef85f7579508654
jdavidzt <jdavidzt9003@gmail.com>
2020-09-24 21:06:17 -0500

    Se realizo retos en javascript-challenge

--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = `https://rickandmortyapi.com/api/character/`;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
-
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
-    }
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) =>{
+    const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = (event) => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status == 200)
+          resolve(xhttp.responseText);
+        else reject(Error(`${API}`));
+      }
+    };
+    xhttp.open('GET', url_api, true);
+    xhttp.send();
+})
   };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
-    });
-  });
-});
+  let dataOne
+  let dataTwo
+fetchData(API)
+  .then (data1 =>{
+    console.log(`Primer Llamado...`)
+    dataOne = JSON.parse(data1)
+    return fetchData(`${API} ${dataOne.results[0].id}`)
+  })
+  .then(data2 =>{
+    console.log(`Segundo Llamado...`)
+    dataTwo = JSON.parse(data2)
+    return fetchData(dataTwo.origin.url)
+  })
+  .then(data3 =>{
+    let dataThree = JSON.parse(data3)
+      console.log(`Tercero Llamado...`)
+      console.log(`Personajes: ${dataOne.info.count}`);
+      console.log(`Primer Personaje: ${dataTwo.name}`);
+      console.log(`Dimensión: ${dataThree.dimension}`);
+  })
+  .catch((url_api)=>{
+    console.log(`Error: ${url_api}`)
+  })
