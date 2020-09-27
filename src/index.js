4338f7f78a60118d0c0f7d26281d93bcb2d9114d
LoreNa <lmosquerag1@gmail.com>
2020-09-27 17:25:19 -0500

    Parte 2 reto JS ok

--- src/index.js
+++ src/index.js
@@ -15,18 +15,18 @@ const fetchData =(url_api, callback) => {
   xhttp.send();
 }
 
-fetchData('${API}', (error1, data1) => {
+fetchData(`${API}`, (error1, data1) => {
   if (error1) return console.error('Error' + ' ' + error1);
   console.log('Primer Llamado...')
-  fetchData('${API} ${data1.results[0].id}', (error2, data2) => {
+  fetchData(`${API}${data1.results[0].id}`, (error2, data2) => {
     if (error2) return console.error(error2);
     console.log('Segundo Llamado...')
     fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes: ${data1.info.count}');
-      console.log('Primer Personaje: ${data2.name}');
-      console.log('Dimensión: ${data3.dimension}');
+      console.log(`Tercero Llamado...`)
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
