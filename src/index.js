8e68131acecc372331b680ef9b86fe2bb0929ea0
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-09-29 16:59:08 -0500

    reto promesas

--- src/index.js
+++ src/index.js
@@ -3,34 +3,41 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = 'https://rickandmortyapi.com/api/character/';
 const xhttp = new XMLHttpRequest();
 
-const fetchData = (url_api, callback) => {
+const fetchdata = url_api => new Promise((resolve,reject)=>{
   xhttp.onreadystatechange =  event => {
     if (xhttp.readyState === 4) {
-      if (xhttp.status == 200){
+      if (xhttp.status == 200)
+      {
         const objectchange = JSON.parse(xhttp.responseText)
-          callback(null, objectchange);
+          resolve(objectchange);
         }
         
-      else return callback(url_api);
+      else reject (url_api);
     }
   };
   xhttp.open('GET', url_api, false);
   xhttp.send();
-};
+}
+)
 
+let data1
+let data2
+  
 
-fetchData(API,  (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log(`Primer Llamado...`)
-  fetchData(API + data1.results[0].id,(error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log(`Segundo Llamado...`)
-    fetchData(data2.origin.url,  (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log( `Dimensión: ${data3.dimension}`);
-    });
-  });
-});
+fetchdata(API)
+.then((data)=>{console.log(`Primer Llamado...`)
+data1=data
+return  fetchdata (API + data.results[0].id)
+})
+.then((data)=> {console.log(`Segundo Llamado...`)
+data2=data
+return fetchdata (data.origin.url)
+})
+.then ((data3)=> {
+  console.log('Tercero Llamado...')
+  console.log(`Personajes: ${data1.info.count}`);
+  console.log(`Primer Personaje: ${data2.name}`);
+  console.log( `Dimensión: ${data3.dimension}`);
+})
+.catch((error)=> console.error(`Error ${error}`))
+
