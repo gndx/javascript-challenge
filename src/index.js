7926ab72837a4b2c90943d7719a19f42bc8c25b5
Esvin Ambrocio <esvinambrocioc@gmail.com>
2021-10-28 21:08:55 -0600

    tercer problema cambiar el codigo ecmascript(es6)

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+import fetch from 'node-fetch';
+let API = 'https://rickandmortyapi.com/api/character/';
 
-var API = 'https://rickandmortyapi.com/api/character/';
-
-function fetchData(url_api, callback) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = () =>{
-    if (xhttp.readyState === xhttp.DONE) {
-      if (xhttp.status == 200){
-        const respose = JSON.parse(xhttp.responseText)
-        callback(null, respose);
+function fetchData(url_api) {
+  return fetch(url_api)
+    .then( response => {
+      if(response.status == 200){
+        return response.json()
       }
-      else return callback(url_api);
-    }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
+      console.log(response)
+    })
+    .catch(error => {
+      console.error(error)
+    });
 };
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(`Error ${error2}`);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, (error3, data3) =>{
-      if (error3) return console.error(`Error ${error3}`);
-      console.log('Tercero Llamado...')
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
-  });
-});
+async function getData (){
+    const first = await fetchData(API);
+    const urlSecond = `${API}${first.results[0].id}`;
+    const second = await fetchData(urlSecond);
+    const urlThird = `${second.origin.url}`;
+    const third = await fetchData(urlThird);
+    console.log(`Personajes: ${first.info.count}`);
+    console.log(`Primer Personaje: ${second.name}`);
+    console.log(`Dimensión: ${third.dimension}`);
+}
+getData()
