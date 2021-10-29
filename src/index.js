b9f1f5154c491a6e8f7091f967d20ad6f5de61c0
Esvin Ambrocio <esvinambrocioc@gmail.com>
2021-10-28 20:16:57 -0600

    Segunda solución cambiar a arrow functions y templeate strings

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function (event) {
+  xhttp.onreadystatechange = () =>{
     if (xhttp.readyState === xhttp.DONE) {
       if (xhttp.status == 200){
         const respose = JSON.parse(xhttp.responseText)
@@ -17,18 +17,18 @@ function fetchData(url_api, callback) {
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(`Error ${error1}`);
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
+  fetchData(API + data1.results[0].id, (error2, data2) => {
+    if (error2) return console.error(`Error ${error2}`);
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
+    fetchData(data2.origin.url, (error3, data3) =>{
+      if (error3) return console.error(`Error ${error3}`);
       console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
